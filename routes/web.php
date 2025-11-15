2685de8dfc7bef9a8fb5feb336613528a457ff82
ghalmi <you@example.com>
2025-11-16 00:01:33 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/pepole', function(){
+
+    $pepole = [
+        ["name" => "mario","skill" => 75, "id" => "1"],
+        ["name" => "luigi","skill" => 45, "id" => "2"],
+    ];
+
+    return view('pepole.index', ["greeting" => "hello" , "pepole" => $pepole]);
+});
+
+Route::get('/pepole/{id}', function($id){
+
+    //fetch record with id
+    return view('pepole.show', ["id" => $id]);
+});
