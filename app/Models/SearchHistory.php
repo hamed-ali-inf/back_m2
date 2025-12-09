7d2b23a1696d7efb6f5dc144c7161346044c3418
ghalmi <you@example.com>
2025-12-09 23:17:53 +0100

    Search History MODEL

---
+++ app/Models/SearchHistory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SearchHistory extends Model
+{
+    protected $table = 'search_history';
+
+    protected $fillable = [
+        'user_id',
+        'query',
+        'result'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
