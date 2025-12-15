ff85ab95623e7d6635e469231973cb6197c15654
ahmed-tdf <djaiz.ahmed@cuniv-tindouf.dz>
2025-12-15 22:28:47 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\SearchController;
 use App\Http\Controllers\KeywordController;
+use App\Http\Controllers\Api\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,3 +26,13 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 Route::post('/keywords/search', [KeywordController::class, 'search']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('/products', [ProductController::class, 'store']);
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::put('/products/{id}', [ProductController::class, 'update']);
+
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
